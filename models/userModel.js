0bebff3c733fe6fe48d46a34d60676171b9078a1
wallandall <wallandall@gmail.com>
2019-12-14 01:03:58 +0100

    Added login and register user#

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please enter a password'],
-    minlength: 6
+    minlength: 6,
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -39,6 +40,13 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
